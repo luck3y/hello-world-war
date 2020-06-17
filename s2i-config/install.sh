b4872df3e1e1e6a87150fcbd8f9856388de203a6
Ken Wills <ken@zaptillion.net>
2020-06-17 15:26:02 -0500

    test

--- s2i-config/install.sh
+++ s2i-config/install.sh
@@ -4,9 +4,9 @@ injected_dir=$1
 
 echo "Running on injected_dir=${injected_dir}"
 
-echo ${WORKDIR}
+echo WORKDIR: ${WORKDIR}
 ls -l /opt/app-root/src
-mount 
+env
 
 echo "Applying patch"
 $JBOSS_HOME/bin/jboss-cli.sh --file=${injected_dir}/patch.cli
