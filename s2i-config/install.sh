5f18b5667674e382282a03cfc27bf99632dbd887
Ken Wills <ken@zaptillion.net>
2020-06-17 14:24:30 -0500

    test

--- s2i-config/install.sh
+++ s2i-config/install.sh
@@ -2,11 +2,6 @@
 
 injected_dir=$1
 
-echo "Copy ${injected_dir}/extensions/postconfigure.sh to ${JBOSS_HOME}/extensions/"
-
-mkdir -p "${JBOSS_HOME}/extensions/"
-cp "${injected_dir}/extensions/postconfigure.sh" "${JBOSS_HOME}/extensions/"
-
-echo "Copy ${injected_dir}/extensions/patch.cli to ${JBOSS_HOME}/extensions/"
-cp "${injected_dir}/extensions/patch.cli" "${JBOSS_HOME}/extensions/"
+echo "Applying patch"
+$JBOSS_HOME/bin/jboss-cli.sh --file=patch.cli
 
